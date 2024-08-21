de74a88d05a212508abcaae918303000868f58e7
Mateus Andre Paiva de Araujo <130513304+MateusPaiva74@users.noreply.github.com>
2024-08-21 11:48:35 -0300

    commit teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import Adicao from './components/Adicao'
-import Divisao from './components/Divisao'
-import Multiplicacao from './components/Multiplicacao'
-import Subtracao from './components/Subtracao'
 import './App.css'
+import Mensagem from './components/Mensagem'
 
 const App = () => {
   return (
     <div className="container">
-      <Adicao num1={32}  num2={3}/>
-      <Divisao num1={6280} num2={12}/>
-      <Multiplicacao num1={257} num2={12}/>
-      <Subtracao num1={3084} num2={677}/>
+      <h1>Renderização condicional simples</h1>
+      <Mensagem tipo="sucesso"/>
+      <Mensagem tipo="erro"/>
+      <Mensagem tipo="aviso"/>
+
     </div>
   )
 }
